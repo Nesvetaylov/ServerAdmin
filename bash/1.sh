4654390d712bfcbc241d8babe9c66188503dfa5b
Saint Juuuly <saintjuuuly@mail.ru>
2025-06-09 09:43:56 +0300

    Change 1.sh

--- bash/1.sh
+++ bash/1.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Проверка количества аргументов
 if [ $# -ne 2 ]; then
     echo "Использование: $0 <имя файла> <время в секундах>"
     exit 1
@@ -9,16 +8,14 @@ fi
 filename="$1"
 duration="$2"
 
-# Проверка, что второй аргумент - целое число
 if ! [[ "$duration" =~ ^[0-9]+$ ]]; then
     echo "Ошибка: второй аргумент должен быть целым числом (секунд)"
     exit 1
 fi
 
-# Основной цикл
 i=0
 while [ $i -lt $duration ]; do
-    timestamp=$(date +%d/%m/%y\ %H:%M:%S)  # Добавлены секунды
+    timestamp=$(date +%d/%m/%y\ %H:%M:%S)  
     loadavg=$(cat /proc/loadavg | awk '{print $1}')
     echo "$timestamp = $loadavg" >> "$filename"
     sleep 1
